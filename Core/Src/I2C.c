e1972e6fdb7a6f35a8ec8750ebdf6bb8de92a60b
Sokolov Eugene <zm-7@inbox.ru>
2023-06-22 17:12:14 +0300

    Написаны настройки для интерфейса I2C

--- Core/Src/I2C.c
+++ Core/Src/I2C.c
@@ -10,21 +10,34 @@
 void I2C_init(){
 /*включение тактирования портов для интерфейса*/
     RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;		//Включил тактирование на шине с интрейсом
+    RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;		//Вкл. тактирование алтернативной функции
     RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;		//Включил тактирование пинов GPIOB
 
  /* Настройка пина Alternative func.open drain
   * SCL -> I2C clock => PB6
   * SDA -> I2C Data I/O => PB7*/
     GPIOB->CRL |= GPIO_CRL_CNF6;	//Open-drain
-    GPIOB->CRL |= GPIO_CRL_MODE6;	//Alternative func Макс 10МГц т.к частота шины 24MHz
+    GPIOB->CRL |= GPIO_CRL_MODE6;	//Alternative func Макс 50МГц т.к частота шины 24MHz
     GPIOB->CRL |= GPIO_CRL_CNF7;
     GPIOB->CRL |= GPIO_CRL_MODE7;
 
-    /*Настройка I2C*/
+    /*Настройка I2C по умолчанию все регистры равны нулю ToDo проверить так ли это? на примере OAR1/2?*/
+    /*Формула Tclk = 1ns/24MHz = 24ns
+     * SM = 1000ns | FM = 300ns
+     * Выбран StandartMode
+     * TRISE = (SM/Tclk) +1 => 25 = 0x19
+     * CCR = Tsm(sm mode freq = 100kHz)/2*Tclk(1/24MHz) = 120 => 0x7A*/
 
     I2C1->CR1 &= ~I2C_CR1_PE;		//Отключаю интерфейс для его настройки
+
     I2C1->CR1 |= I2C_CR1_SWRST;		//Перезагружаю
     I2C1->CR1 &= ~I2C_CR1_SWRST;	//Действия аналогично HAL
+
     I2C1->CR2 |= 0x18; 			//HEX(18) = 24MHz шины APB1
-    //I2C1->TRISE =
+    I2C1->TRISE = 0x19;			//Время нарастания фронта сигнала
+
+    I2C1->CCR &= ~I2C_CCR_FS;		//Настройка режима мастер SM = 0
+    I2C1->CCR |= 0x7A;			//Задаю тактирование для SCL
+
+    I2C1->CR1 |= I2C_CR1_PE;		//Включение интерфейса
 }
