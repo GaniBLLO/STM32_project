a1dd75c67a0f6edf7a461edc9909084fb56e1077
EvgenSokol <Zm-7@inbox.ru>
2023-09-17 20:05:11 +0300

    Update I2C.c
    Переделал инициализацию под новую скорость.

--- Core/Src/I2C.c
+++ Core/Src/I2C.c
@@ -17,32 +17,32 @@ void I2C_init(){
   * SDA -> I2C Data I/O => PB7	Alternative func.open drain*/
     GPIOB->CRL &= ~GPIO_CRL_CNF6;	//Обнулил регистр
     GPIOB->CRL |= GPIO_CRL_CNF6;	//Open-drain
-    GPIOB->CRL |= GPIO_CRL_MODE6;	//Alternative func Макс 50МГц т.к частота шины 24MHz
+    GPIOB->CRL |= GPIO_CRL_MODE6;	//Alternative func Макс 50МГц т.к частота шины
 
     GPIOB->CRL &= ~GPIO_CRL_CNF7;
     GPIOB->CRL |= GPIO_CRL_CNF7;
     GPIOB->CRL |= GPIO_CRL_MODE7;
 
     /*Настройка I2C по умолчанию все регистры равны нулю ToDo проверить так ли это? на примере OAR1/2?*/
-    /*Формула Tclk = 1ns/24MHz = 24ns
+    /*Формулы
+     * Tclk = 1ns/24MHz = 24ns
      * SM = 1000ns | FM = 300ns
      * Выбран StandartMode
-     * TRISE = (SM/Tclk) +1 => 25 = 0x19
-     * CCR = Tsm(sm mode freq = 100kHz)/2*Tclk(1/24MHz) = 120 => 0x7A*/
+     * TRISE = (SM/Tclk) +1 => 0x25
+     * CCR = Tsm(36MHz - частота шины APB)/2*10kHz - стандратная скорость = 180 => 0xB4*/
 
     I2C1->CR1 &= ~I2C_CR1_PE;		//Отключаю интерфейс для его настройки
-    I2C1->CR1 |= I2C_CR1_SWRST;		//Перезагружаю
-    delay_time(10);
+    I2C1->CR1 |= I2C_CR1_SWRST;		//Перезагружаю для обнуления регистров
+    delay_time(5);
     I2C1->CR1 &= ~I2C_CR1_SWRST;	//Действия аналогично HAL
-    delay_time(10);
-
+    delay_time(5);
     I2C1->CR1 &= ~I2C_CR1_SMBUS;	//Режим I2C
 
-    I2C1->CR2 = 0x24; 			//HEX(24) = 36MHz шины APB1
-    I2C1->TRISE = 0x25;			//Время нарастания фронта сигнала
+    I2C1->CR2 = 0x24; 				//HEX(24) = 36MHz (MAX для шины APB1)
 
-    I2C1->CCR &= ~I2C_CCR_FS;		//Настройка режима мастер SM = 0
-    I2C1->CCR = 0x7A;			//Задаю тактирование для SCL
+    I2C1->CCR &= ~I2C_CCR_FS;		//Включил стандартный режи работы интерфейса
+    I2C1->CCR = 0xB4;				//Задаю тактирование для SCL
+    I2C1->TRISE = 0x25;				//Время нарастания фронта сигнала
 
     I2C1->CR1 |= I2C_CR1_PE;		//Включение интерфейса
 }
@@ -51,8 +51,8 @@ uint8_t I2C_send(uint8_t address, uint8_t command){
 
     uint8_t f_ready = 0;
     I2C1->CR1 &= ~I2C_CR1_POS;		/*Этот бит ставится что бы показать,
-					 *что след.бит будет принят в регистр -> DR
-					 *для его дальнейшего смещения*/
+					 	 	 	 	 *что след.бит будет принят в регистр -> DR
+					 	 	 	 	 *для его дальнейшего смещения*/
     I2C1->CR1 |= I2C_CR1_START;		//Генерируем стартовый бит
     while(!(I2C1->SR1 & I2C_SR1_SB));	//Ждём пока регситр не сгенерирует бит старта. 0 => ждём... 1 = > готово!
 
